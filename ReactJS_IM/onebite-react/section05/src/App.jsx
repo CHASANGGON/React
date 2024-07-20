a3b45eafa85fd41ef689a8f5b8243f4e61c009f0
CHASANGGON <yg9618@naver.com>
2024-07-21 01:01:47 +0900

    ReactJS IM 5

--- ReactJS_IM/onebite-react/section05/src/App.jsx
+++ ReactJS_IM/onebite-react/section05/src/App.jsx
@@ -1,28 +1,11 @@
 import "./App.css";
-import Header from "./components/Header";
-import Main from "./components/Main";
-import Footer from "./components/Footer";
-import Button from "./components/Button";
+import HookExam from "./components/HookExam";
+import Register from "./components/Register";
 
-// 부모 컴포넌트
-// App 컴포넌트는 조상 컴포넌트(상위 컴포넌트)
-// root 컴포넌트가 최상위 컴포넌트
 function App() {
-  const buttonProps = {
-    text: "메일",
-    color: "red",
-    a: 1,
-    b: 1,
-    c: 1,
-  };
   return (
     <>
-      <Button {...buttonProps} />
-      <Button text={"카페"} color={"blue"} />
-      <Button text={"블로그"}>
-        <div>자식요소</div>
-        <Header />
-      </Button>
+      <HookExam />
     </>
   );
 }
