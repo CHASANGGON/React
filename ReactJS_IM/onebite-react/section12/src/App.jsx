fdf6e2c56c4bdf7f9aa74d642ee43c980cdd5ee1
차상곤 <yg9618@naver.com>
2024-07-23 17:59:38 +0900

    ReactJS IM 12

--- ReactJS_IM/onebite-react/section12/src/App.jsx
+++ ReactJS_IM/onebite-react/section12/src/App.jsx
@@ -1,56 +1,56 @@
 import "./App.css";
+import { useReducer } from "react";
 import Home from "./pages/Home.jsx";
 import New from "./pages/New.jsx";
 import Diary from "./pages/Diary.jsx";
+import Edit from "./pages/Edit.jsx";
 import Notfound from "./pages/Notfound.jsx";
-import { Route, Routes, Link, useNavigate } from "react-router-dom";
-import Button from "./components/Button.jsx";
-import Header from "./components/Header.jsx";
+import { Route, Routes } from "react-router-dom";
 
-import { getEmotionImage } from "./util/get-emotion-image.js";
+const mockData = [
+  {
+    id: 1,
+    createdData: new Date().getTime(),
+    emotionId: 1,
+    content: "1번 일기 내용",
+  },
+  {
+    id: 2,
+    createdData: new Date().getTime(),
+    emotionId: 2,
+    content: "2번 일기 내용",
+  },
+];
 
-// 1. "/" : 모든 일기를 조회하는 Home 페이지
-// 2. "/new" : 새로운 일기를 작성하는 New 페이지
-// 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
+function reducer(state, action) {
+  return state;
+}
+
 function App() {
-  const nav = useNavigate();
+  const [data, dispatch] = useReducer(reducer, mockData);
+
+  // 새로운 일기 추가
+  const onCreate = (createdData, emotionId, content) => {
+    dispatch({
+      type: "CREATE",
+      data: {
+        createdData,
+        emotionId,
+        content,
+      },
+    });
+  };
 
   const onClickButton = () => {
     nav("/new");
   };
   return (
     <>
-      <Header
-        title={"Header"}
-        leftChild={<Button text={"Left"} />}
-        rightChild={<Button text={"Right"} />}
-      />
-
-      <Button
-        text={"123"}
-        onClick={() => {
-          console.log("버튼 클릭");
-        }}
-      />
-      <Button
-        text={"123"}
-        type={"POSITIVE"}
-        onClick={() => {
-          console.log("버튼 클릭");
-        }}
-      />
-      <Button
-        text={"123"}
-        type={"NEGATIVE"}
-        onClick={() => {
-          console.log("버튼 클릭");
-        }}
-      />
-
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/new" element={<New />}></Route>
         <Route path="/diary/:id" element={<Diary />}></Route>
+        <Route path="/edit/:id" element={<Edit />}></Route>
         <Route path="*" element={<Notfound />}></Route>
       </Routes>
     </>
