deca7e94f0dd607f27629ac2de79fb2c3639b5a5
차상곤 <yg9618@naver.com>
2024-07-24 18:00:39 +0900

    ReactJS Review

--- ReactJS_IM/onebite-react/section02/chapter06.js
+++ ReactJS_IM/onebite-react/section02/chapter06.js
@@ -3,17 +3,17 @@ let arr = [1, 2, 3];
 
 // 1.1 배열 인덱스
 for (let i = 0; i < arr.length; i++) {
-  //   console.log(arr[i]);
+    console.log(arr[i]);
 }
 
 let arr2 = [4, 5, 6, 7, 8];
 for (let i = 0; i < arr2.length; i++) {
-  //   console.log(arr2[i]);
+    console.log(arr2[i]);
 }
 
 // 1.2 for of 반복문
 for (let item of arr) {
-  //   console.log(item);
+    console.log(item);
 }
 
 // 2. 객체 순회
--- ReactJS_IM/onebite-react/section02/chapter13.js
+++ ReactJS_IM/onebite-react/section02/chapter13.js
@@ -1,69 +1,30 @@
-// 인자(인수)로는 비동기 작업을 실제로 진행할 콜백함수
-function add10(num) {
-  const promise = new Promise((resolve, reject) => {
-    // 성공과 실패 상태로 바꾸는 resolve, reject 함수
+const promise = new Promise((resolve, reject) => {
+  // 성공과 실패 상태로 바꾸는 resolve, reject 함수
 
-    // 비동기 작업 실행하는 함수
-    // executor
+  // 비동기 작업 실행하는 함수
+  // executor
 
-    setTimeout(() => {
-      // const num = 10;
+  setTimeout(() => {
+    // const num = 10;
 
-      if (typeof num === "number") {
-        resolve(num + 10);
-      } else {
-        reject("num이 숫자가 아닙니다.");
-      }
+    if (typeof num === "number") {
+      resolve(num + 10);
+    } else {
+      reject("num이 숫자가 아닙니다.");
+    }
+  }, 2000);
+});
+return promise;
+console.log(promise); // pending(대기)
 
-      // console.log("안녕");
-
-      // resolve("안녕");
-      // PromiseState : pending -> "fulfilled"
-      // PromiseResult : "안녕"
-
-      // reject("왜 실패했는지 이유...");
-      // PromiseState : pending -> "rejected"
-      // PromiseResult : "왜 실패했는지 이유..."
-    }, 2000);
-  });
-  return promise;
-}
-// console.log(promise); // pending(대기)
 // then 메서드
-// -> 그 후에
 // Promise가 성공했을 경우 실행
-// promise.then((value) => {
-//   console.log(value);
-// });
+promise.then((value) => {
+  console.log(value);
+});
 
-// // Promise가 실패했을 경우 실행
-// promise.catch((error) => {
-//   console.log(erroe);
-// });
-
-// // 이어붙여서 실행도 가능
-// promise
-//   .then((value) => {
-//     console.log(value);
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   });
-
-
-add10(0)
-  .then((result) => {
-    console.log(result);
-    return add10(result); // 성공하면 promise 객체를 반환
-  })
-  .then((result) => {
-    console.log(result);
-    return add10(result);
-    // return add10(undefined);
-  })
-  .then((result) => {
-    console.log(result);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+// catch 메서드
+// Promise가 실패했을 경우 실행
+promise.catch((error) => {
+  console.log(erroe);
+}); 
